fc15d86fc210a3ca4bd06aba6ef99ff342ce68b3
lorenzocazzador <lori.rolli.19@gmail.com>
2020-12-02 08:52:14 +0100

    small changes day2

--- 2020/02/1.py
+++ 2020/02/1.py
@@ -2,7 +2,7 @@ import sys
 ans = 0
 for l in sys.stdin.read().strip().split('\n'):
     x = l.split(' ')
-    v  = list(map(int,x[0].split('-')))
+    v = list(map(int,x[0].split('-')))
     c = x[1][0]
     w = x[2]
     ans += w.count(c) >= v[0] and w.count(c) <= v[1]
