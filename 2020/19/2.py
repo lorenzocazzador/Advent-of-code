ab352d0d3e8bf61431decc1bfacbe20b1d6d94f7
lorenzocazzador <lori.rolli.19@gmail.com>
2020-12-19 14:48:12 +0100

    day19 done

--- 2020/19/2.py
+++ 2020/19/2.py
@@ -4,10 +4,8 @@ r, m = sys.stdin.read().strip().split('\n\n')
 def f(r):
     if rul[r][0] == 0:
         return [rul[r][1]]
-
     if r in memo:
         return memo[r]
-
     res = []
     # for every rules
     for x in rul[r][1]:
@@ -24,11 +22,34 @@ def f(r):
     memo[r] = res
     return res
 
+def isValid(s):
+    # 0: 8 11
+    # 8: 42 | 42 8
+    # 11: 42 31 | 42 11 31
+    a, b = f(42), f(31)
+    firstHalf = True
+    counta = countb = 0
+    for i in range(0, len(s), len(a[0])):
+        sub = s[i:i+len(a[0])]
+        if firstHalf:
+            if sub in a:
+                counta += 1
+            elif sub in b:
+                countb += 1
+                firstHalf = False
+            else:
+                return False
+        else:
+            if sub not in b:
+                return False
+            countb += 1
+    return counta > countb and counta > 1 and countb > 0
 
 rul = {}
 for x in r.split('\n'):
     n, l = x.split(': ')
     n = int(n)
+
     if '"' in l:
         rul[n] = (0, l[1])
     else:
@@ -38,6 +59,5 @@ for x in r.split('\n'):
         rul[n] = (1, k)
 
 memo = {}
-p = f(0)
-res = sum([i in p for i in m.split('\n')])
+res = sum([isValid(i) for i in m.split('\n')])
 print(res)
